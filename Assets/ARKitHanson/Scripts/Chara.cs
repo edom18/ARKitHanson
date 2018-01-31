18b5b156f7b99286fe7d12766ff5c116ee8b5b35
edom18 <edo.m18@gmail.com>
2018-01-31 11:36:51 +0900

    Added chara collision detection and lookat in collision.

---
+++ Assets/ARKitHanson/Scripts/Chara.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chara : MonoBehaviour
+{
+	private HeadLookController _headLookController;
+
+	private void Awake()
+	{
+		_headLookController = GetComponent<HeadLookController>();
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.CompareTag("Player"))
+		{
+			LookAt(collider.transform);
+		}
+	}
+
+	private void LookAt(Transform target)
+	{
+		Debug.Log("LookAt");
+		_headLookController.lookTarget = target;
+	}
+}
